2bcf4c125c26eaec3421d80aa41f7dc74153e899
Perawih Jarunith <perawithjarunithi@gmail.com>
2016-05-27 10:40:40 +0700

    fix again

--- hadoop/views.py
+++ hadoop/views.py
@@ -53,7 +53,7 @@ class JobViewList(APIView):
             job = serializer.save()
             file = open(str(job.id)+'.txt', 'w')
             file.write(request.data['input'])
-            command = str("/bin/bash;/home/ubuntu/pig-0.15.0/bin/pig -x local -param user_input=\"/home/ubuntu/WebAppStupidServer/"+str(job.id)+".txt\" -param output_path=/home/ubuntu/output/"+str(job.id)+" -f /home/ubuntu/sentence_search.pig")
+            command = str("/bin/bash -i -c /home/ubuntu/pig-0.15.0/bin/pig -x local -param user_input=\"/home/ubuntu/WebAppStupidServer/"+str(job.id)+".txt\" -param output_path=/home/ubuntu/output/"+str(job.id)+" -f /home/ubuntu/sentence_search.pig")
             file.close()
             self.execute(command)
             job = Job.objects.get(id=job.id)
